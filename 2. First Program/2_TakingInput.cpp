2d8d4575712ed7dbdf3221ba0d47365180a61e5b
Muhammad Shayan <21sw067@students.muet.edu.pk>
2023-04-29 20:00:25 +0100

    taking input

---
+++ 2. First Program/2_TakingInput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+int main()
+{
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "You entered: " << number << std::endl;
+
+
+    return 0;
+}
